53a8226256cfccd7789b9cf7053f784435d3cae5
olssonolof <44023615+olssonolof@users.noreply.github.com>
2018-10-11 16:29:38 +0200

    Added Classes

---
+++ shopProject/shopProject/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace shopProject
+{
+    class Product
+    {
+        public string Name;
+        public int Year;
+        public int Price;
+
+
+
+        public void GetData()
+        {
+            string[] nonFormated = File.ReadAllLines("products.csv");
+            foreach  (string line in nonFormated)
+            {
+                string[] x = line.Split(',');
+                Name = x[0];
+                Year = int.Parse(x[1]);
+                Price = int.Parse(x[2]);
+            }
+        }
+    }
+    class person
+    {
+
+    }
+}
