c6f704c3cfbe5dd90ffaf7deda1d10668ecff2aa
olssonolof <44023615+olssonolof@users.noreply.github.com>
2018-10-12 13:48:22 +0200

    Added Customer Class

--- shopProject/shopProject/Product.cs
+++ shopProject/shopProject/Product.cs
@@ -12,20 +12,32 @@ namespace shopProject
         public string Name;
         public int Year;
         public int Price;
-
-
-
+               
         public Product(string formated)
         {
             string [] x = formated.Split(',');
           
             Name = x[0];
             Year = int.Parse(x[1]);
-            Price = int.Parse(x[2]);
-                                            
-        }
+            Price = int.Parse(x[2]);                                            
+        }        
+    }
+    class Customer
+    {
+        public List<Product> Cart;
+        public int TotalPrise;
+        public int TotalNrOfProduct;
+        public int Discount;
 
-        
+
+        public void AddProductToCard(Product product, int amount)
+        {
+            for (int i = 1; i < amount; i++)
+            {
+                Cart.Add(product);
+                TotalPrise += product.Price;
+            }
+        }
     }
    
 }
